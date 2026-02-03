e2075273480424ce5ec03f449b0a979821d38783
Una <una.lestrange@atu.ie>
2026-02-03 15:27:27 +0000

    removed the method code and just left stubs

--- WS3Code/Program.cs
+++ WS3Code/Program.cs
@@ -4,78 +4,29 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
-            Console.WriteLine(GetLargestIndex([9, 9, 9]));
-            Console.WriteLine(GetLargestIndex([9, 9, 9, 10, 9, 10, 9, 11, 3, 11, 5]));
-
-
-            int[] test1 = [67, 45, 64, 34, 456, 45];
-            int[] expected = [67, 45, 64, 34, 45, 456];
-            WS3Code.Program.MoveLargestToLast(test1);
+           
 
         }
 
 
         public static int GetLargestValue(int[] values)
         {
-            int largest;
-            if (values.Length == 0)
-            {
 
-                largest = 0;
-            }
-            else
-            {
-                largest = values[0];
 
-                for (int i = 1; i < values.Length; i++)
-                {
-                    if (values[i] > largest)
-                    {
-                        largest = values[i];
-                    }
-                }
-            }
 
-            return largest;
+            return 0;
         }
 
         public static int GetSmallestValue(int[] values)
         {
-            int smallest = 0;
-            if (values.Length == 0)
-            {
 
-                smallest = 0;
-            }
-            else
-            {
-                smallest = values[0];
 
-                for (int i = 1; i < values.Length; i++)
-                {
-                    if (values[i] < smallest)
-                    {
-                        smallest = values[i];
-                    }
-                }
-            }
-
-            return smallest;
+            return 0;
         }
 
         public static bool IsAscending(int[] values)
         {
 
-
-            for (int i = 0; i < values.Length - 1; i++)
-            {
-                if (values[i] > values[i + 1])
-                {
-                    return false;
-                }
-            }
 
             return true;
         }
@@ -90,35 +41,12 @@
         {
             int answer = 0;
 
-            if (values.Length > 1)
-            {
-                int largest = values[0];
-
-                for (int i = 1; i < values.Length; i++)
-                {
-                    if (values[i] >= largest)
-                    {
-                        largest = values[i];
-                        answer = i;
-                    }
-                }
-            }
             return answer;
         }
 
         public static void MoveLargestToLast(int[] values)
         {
-            if (values.Length > 1)
-            {
-                int largestIndex = GetLargestIndex(values);
-                int temporary = values[largestIndex];
-                for (int i = largestIndex; i < values.Length - 1; i++)
-                {
-                    values[i] = values[i + 1];
-                }
-                values[values.Length-1] = temporary;
 
-            }
         }
     }
 }
